7cb22c6e2495b56665a54faaddb9853a35026630
mahmoud3ali <you@example.com>
2020-03-05 21:56:32 +0200

    fix(users): server crashed due to bad import

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const auth = require("../middleware/authenticate");
+const auth = require("../middleware/authorize");
 const jwt = require("jsonwebtoken");
 const { BAD_REQUEST, OK, INTERNAL_SERVER_ERROR } = require("http-status");
 const { JWT_SECRET } = process.env;
