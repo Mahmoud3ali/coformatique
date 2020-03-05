ad58d96f1d2adb4f0bb8fa511d0147948ba938c7
mahmoud3ali <you@example.com>
2020-03-05 21:19:50 +0200

    feat(users): added new endpoint to create user

--- src/router.js
+++ src/router.js
@@ -3,9 +3,14 @@ const { OK } = require("http-status");
 
 const { version } = require("../package.json");
 
+const UsersRouter = require("./routes/users");
+
 const router = new Router();
+router.use("/users", UsersRouter);
 
 // maintenance endpoint
-router.get('/system-info', (req, res, next) => res.status(OK).json({ backendVersion: version }));
+router.get("/system-info", (req, res, next) =>
+  res.status(OK).json({ backendVersion: version })
+);
 
 module.exports = router;
---
+++ src/routes/users.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const {
+  BAD_REQUEST,
+  CONFLICT,
+  CREATED,
+  INTERNAL_SERVER_ERROR
+} = require("http-status");
+const { check, validationResult } = require("express-validator");
+
+const { User } = require("../models");
+
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 8 or more characters"
+    ).isLength({ min: 8 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(BAD_REQUEST).send({
+        message: { errors: errors.array() }
+      });
+    }
+
+    const { name, email, password } = req.body;
+    delete req.body.password;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(CONFLICT).send({ message: "Email already exists" });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      res.status(CREATED).send({ message: "Created Successfully" });
+    } catch (err) {
+      console.error(err.message);
+      res.status(INTERNAL_SERVER_ERROR).send({ message: "Server error" });
+    }
+  }
+);
+
+module.exports = router;
