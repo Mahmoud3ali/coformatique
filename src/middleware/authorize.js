eb9f10e40b582e520a611f97e62fd508258ea611
mahmoud3ali <you@example.com>
2020-03-06 19:42:54 +0200

    fix(auth): fix bug of reading the token from bad header

--- src/middleware/authorize.js
+++ src/middleware/authorize.js
@@ -1,21 +1,21 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
-const { BAD_REQUEST, INTERNAL_SERVER_ERROR } = require("http-status");
+const { UNAUTHORIZED, INTERNAL_SERVER_ERROR } = require("http-status");
 
 module.exports = async function(req, res, next) {
   // Get token from header
-  const token = req.header("x-auth-token");
+  const token = req.header("Authorization").substring(7);
 
   // Check if there's no token
   if (!token) {
-    return res.status(BAD_REQUEST).json({ message: "Invalid token" });
+    return res.status(UNAUTHORIZED).json({ message: "Unauthorized" });
   }
 
   // Verify token
   try {
     await jwt.verify(token, JWT_SECRET, (error, decoded) => {
       if (error) {
-        res.status(BAD_REQUEST).json({ message: "Invalid token" });
+        res.status(UNAUTHORIZED).json({ message: "Unauthorized" });
       } else {
         req.user = decoded.user;
         next();
