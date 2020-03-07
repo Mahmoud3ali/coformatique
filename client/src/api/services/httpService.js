ee5f1293b448a26276560e09861934548f94071e
mahmoud3ali <you@example.com>
2020-03-08 00:44:02 +0200

    feat: configured axios

---
+++ client/src/api/services/httpService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+axios.interceptors.response.use(null, error => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    console.error(error);
+  }
+
+  return Promise.reject(error);
+});
+
+const setJwt = jwt => {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+};
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  patch: axios.patch,
+  delete: axios.delete,
+  setJwt
+};
