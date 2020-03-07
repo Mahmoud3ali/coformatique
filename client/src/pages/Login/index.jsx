6e70674d17d168c60be34a6c8f008fa8b91e24a3
mahmoud3ali <you@example.com>
2020-03-07 23:46:03 +0200

    feat(auth): added login page ui

---
+++ client/src/pages/Login/index.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+
+import {
+  Grid,
+  Typography,
+  TextField,
+  withStyles,
+  Button
+} from "@material-ui/core";
+
+const LoginScreen = ({ classes }) => {
+  const [userLoginData, setLoginData] = useState({
+    email: "",
+    password: ""
+  });
+  return (
+    <Grid
+      container
+      direction="column"
+      alignItems="stretch"
+      className={classes.container}
+      spacing={8}
+    >
+      <Grid item>
+        <Typography
+          variant="h1"
+          color="textPrimary"
+          align="center"
+          className={classes.header}
+        >
+          Guestbook
+        </Typography>
+      </Grid>
+      <Grid item>
+        <TextField
+          placeholder="Email"
+          type="email"
+          fullWidth
+          value={userLoginData.email}
+          onChange={e => {
+            setLoginData({ ...userLoginData, email: e.target.value });
+          }}
+          variant="outlined"
+          autoComplete="email"
+          classes={{
+            root: classes.textFieldRoot
+          }}
+          InputProps={{
+            classes: {
+              notchedOutline: classes.notchedOutline
+            }
+          }}
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          placeholder="Password"
+          type="password"
+          fullWidth
+          value={userLoginData.password}
+          onChange={e => {
+            setLoginData({
+              ...userLoginData,
+              password: e.target.value
+            });
+          }}
+          variant="outlined"
+          autoComplete="password"
+          classes={{
+            root: classes.textFieldRoot
+          }}
+          InputProps={{
+            classes: {
+              notchedOutline: classes.notchedOutline
+            }
+          }}
+        />
+      </Grid>
+      <Grid item align="center">
+        <Button variant="outlined" onClick={() => {}}>
+          Login
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+const styles = theme => ({
+  header: {
+    fontWeight: "bold"
+  },
+  container: {
+    height: "100%",
+    alignContent: "center",
+    marginTop: theme.spacing(10)
+  }
+});
+
+export default withStyles(styles)(LoginScreen);
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,1 +1,2 @@
+export { default as LoginScreen } from './Login';
 export { default as NotFoundScreen } from './NotFound';
